e9b74f0f3b34dc6fcfb37a4014aca075c1f1e949
iambahyedddinorj <bahyeddinsubasi51@gmail.com>
2025-12-03 23:57:12 +0300

    fix: Hardcode Firebase config
    The Firebase configuration details were previously loaded from environment variables. This commit hardcodes these values directly into the `firebase.ts` file to simplify setup and ensure consistent configuration across environments. This change removes the dependency on `import.meta.env` for these specific values.

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAPuNKIyVKwGHWaPwYlb1Lu9irdBLcdkic",
+  authDomain: "oto-parca-pro.firebaseapp.com",
+  projectId: "oto-parca-pro",
+  storageBucket: "oto-parca-pro.firebasestorage.app",
+  messagingSenderId: "41893729916",
+  appId: "1:41893729916:web:6b024efb76efe481031bf9",
+  measurementId: "G-WDYM78DQ2D"
 };
 
 const app = initializeApp(firebaseConfig);
